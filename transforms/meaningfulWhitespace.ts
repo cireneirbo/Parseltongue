250528dd74da5f3ee21ac37faf7587fb961d09b8
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2020-08-17 01:22:13 -0400

    More cleaning

--- transforms/meaningfulWhitespace.ts
+++ transforms/meaningfulWhitespace.ts
@@ -10,23 +10,5 @@ export default function(sourceFile) {
 		newText += "\n}";
 	}
 
-	// PLEASE DONT DELETE WE MAY USE LATER FOR MORE INTERESTING CASE SCENARIOS
-	// console.log(sourceFile.getFullText());
-	// let keywordCount = {
-	// 	"while": 0,
-	// 	"if": 0
-	// };
-	// // source
-	// const sourceFileSplitted = sourceFile.getFullText().split(" ");
-	// let sourceFileTransformed = "";
-	// for (let i = 0; i < sourceFileSplitted.length; i++) {
-	// 	if (sourceFileSplitted[i].includes("while")) {
-	// 		sourceFileSplitted[i] = sourceFileSplitted[i].replace("while", "while (");
-	// 		keywordCount["while"] += 1;
-	// 	}
-	// 	sourceFileTransformed += " " + sourceFileSplitted[i];
-	// 	console.log(i);
-	// }
-
 	sourceFile.replaceWithText(newText);
 }

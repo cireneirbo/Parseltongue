31670b12f119fea8dd138a37f5a5820c36533a8b
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2020-08-19 00:57:31 -0400

    Refactored module and accounting for more than one replacement

--- transforms/meaningfulWhitespace.ts
+++ transforms/meaningfulWhitespace.ts
@@ -1,10 +1,16 @@
 import { SyntaxKind } from "../lib/typescript/SyntaxKind";
 
 export default function(sourceFile) {
-	// const oldText = sourceFile.getFullText();
-	let newText = sourceFile.getFullText().replace("while ", "while (").replace(":", ") {").replace("elif ", "} else if (");
-	newText = newText.replace(" and ", " && ").replace("if ", "if (").replace(" or ", " || ").replace("for ", "for (");
-	newText = newText.replace("else ", "} else {");
+	const oldText = sourceFile.getFullText();
+	let newText = sourceFile.getFullText().replace("while ", "while (");
+	newText = newText.replace(/:/g, ") {");
+	newText = newText.replace(/elif /g, "} else if (");
+	newText = newText.replace(/ and /g, " && ");
+	newText = newText.replace(/if /g, "if (");
+	newText = newText.replace(/ or /g, " || ");
+	newText = newText.replace(/for /g, "for (");
+	newText = newText.replace(/else /g, "} else {");
+	newText = newText.replace(/ True /g, " true ");
 	if (newText.slice(-1) === "\n") {
 		newText += "}";
 	} else {

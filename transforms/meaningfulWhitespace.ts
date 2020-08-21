13ce017089f9f86ad136c422ecbfbe010a6da45c
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-08-21 16:11:47 -0400

    minor alteration for printout

--- transforms/meaningfulWhitespace.ts
+++ transforms/meaningfulWhitespace.ts
@@ -1,7 +1,8 @@
 import { SyntaxKind } from "../lib/typescript/SyntaxKind";
 
 export default function(sourceFile) {
-	// const oldText = sourceFile.getFullText();
+	const oldText = sourceFile.getFullText();
+	console.log(oldText);
 	let newText = sourceFile.getFullText().replace("while ", "while (").replace(":", ") {").replace("elif ", "} else if (");
 	newText = newText.replace(" and ", " && ").replace("if ", "if (").replace(" or ", " || ").replace("for ", "for (");
 	newText = newText.replace("else ", "} else {");

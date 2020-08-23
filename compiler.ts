8c417654cd1288f678d968fb949675c1e4f58b46
Brian Jenkins <brianjenkins94@gmail.com>
2020-08-23 08:41:42 -0400

    Nested if statement success

--- compiler.ts
+++ compiler.ts
@@ -6,16 +6,19 @@ import { Project } from "ts-morph";
 // Example usage:
 //  npx ts-node ./compiler.ts input/twoPlusTwo.pt
 
-const project = new Project({
-	"addFilesFromTsConfig": false,
-	"tsConfigFilePath": path.join(__dirname, "tsconfig.json")
-});
-
 export function compile(input) {
 	// Normalize whitespace
 	input = input.replace(/^\t+/gm, function(match) {
 		return " ".repeat(match.length * 4);
 	}).replace(/[ \t]+$/gm, "");
+
+	const project = new Project({
+		"addFilesFromTsConfig": false,
+		"tsConfigFilePath": path.join(__dirname, "tsconfig.json"),
+		"compilerOptions": {
+			"alwaysStrict": false
+		}
+	});
 
 	const sourceFile = project.createSourceFile("input.ts", input, { "overwrite": true });
 
@@ -23,7 +26,7 @@ export function compile(input) {
 		const transformFunction = require(path.join(__dirname, "transforms", transform))["default"];
 
 		if (transformFunction.name !== "skip") {
-			transformFunction(sourceFile);
+			transformFunction(sourceFile.getChildren()[0].getChildren()[0]);
 		}
 	}
 
--- transforms/meaningfulWhitespace.ts
+++ transforms/meaningfulWhitespace.ts
@@ -1,99 +1,109 @@
 import { SyntaxKind } from "../lib/typescript/SyntaxKind";
 
+import { compile } from "../compiler";
+
 function getIndentationWidth(text) {
 	return (/^ {2,}/m.exec(text) || [""])[0].length;
 }
 
-export default function(sourceFile) {
-	sourceFile.forEachChild(function recurse(node) {
-		switch (node.getKind()) {
-			case SyntaxKind.DoStatement:
-				throw new Error("Not yet implemented.");
+export default function recurse(node) {
+	//sourceFile.forEachChild(function recurse(node) {
+	switch (node.getKind()) {
+		case SyntaxKind.DoStatement:
+			throw new Error("Not yet implemented.");
 
-				break;
-			case SyntaxKind.ForInStatement:
-			case SyntaxKind.ForOfStatement:
-			case SyntaxKind.ForStatement:
-			case SyntaxKind.IfStatement:
-			case SyntaxKind.WhileStatement:
-				let currentNode = node;
+			break;
+		case SyntaxKind.ForInStatement:
+		case SyntaxKind.ForOfStatement:
+		case SyntaxKind.ForStatement:
+		case SyntaxKind.IfStatement:
+		case SyntaxKind.WhileStatement:
+			let currentNode = node;
 
-				const block = (function recurse(nodeText) {
-					currentNode = currentNode.getNextSibling();
+			const block = (function(nodeText) {
+				currentNode = currentNode.getNextSibling();
 
-					if (nodeText.endsWith(" and") || nodeText.endsWith(" or")) {
-						for (; !currentNode.getPreviousSibling().getFullText().includes(":"); currentNode = currentNode.getNextSibling()) {
-							nodeText += " " + currentNode.getFullText().trim();
-						}
-
-						nodeText = nodeText.replace(/ and /g, " && ").replace(/ or /g, " || ");
+				if (nodeText.endsWith(" and") || nodeText.endsWith(" or")) {
+					for (; !currentNode.getPreviousSibling().getFullText().includes(":"); currentNode = currentNode.getNextSibling()) {
+						nodeText += " " + currentNode.getFullText().trim();
 					}
 
-					const [identifier, condition, rest] = /([a-z]+) (.*?):(.*)/s.exec(nodeText).slice(1);
+					nodeText = nodeText.replace(/ and /g, " && ").replace(/ or /g, " || ");
+				}
 
-					const statements = [];
+				const [identifier, condition, rest] = /([a-z]+) (.*?):(.*)/s.exec(nodeText).slice(1);
 
-					const blockIndentationWidth = getIndentationWidth(rest);
+				const statements = [rest];
 
-					// [!] Problem
-					for (let nextSibling = currentNode.getNextSibling(), nodeText = rest.slice(1); nextSibling !== undefined; nextSibling = nextSibling?.getNextSibling(), nodeText = nextSibling?.getFullText()) {
-						for (const line of nodeText.split(/\r?\n/g)) {
-							if (line === "") {
-								continue;
-							}
+				const blockIndentationWidth = getIndentationWidth(rest);
 
-							const currentIndentationWidth = getIndentationWidth(line);
+				for (let nextSibling = currentNode; nextSibling !== undefined; nextSibling = nextSibling?.getNextSibling()) {
+					for (const line of nextSibling.getFullText().split(/\r?\n/g)) {
+						if (line === "") {
+							continue;
+						}
 
-							if (currentIndentationWidth === blockIndentationWidth) {
-								currentNode = nextSibling;
+						const currentIndentationWidth = getIndentationWidth(line);
 
-								statements.push(line);
-							} else if (currentIndentationWidth === blockIndentationWidth + 4) {
-								currentNode = nextSibling;
+						if (currentIndentationWidth === blockIndentationWidth) {
+							currentNode = nextSibling;
 
-								statements.push(recurse(line));
-							} else {
-								nextSibling = undefined;
+							statements.push(line);
+						} else {
+							nextSibling = undefined;
 
-								break;
-							}
+							break;
 						}
 					}
-
-					return identifier + " (" + condition + ") {\n" + (statements as string[]).join("\n") + "\n}\n";
-				})(currentNode.getFullText());
-
-				// SourceFile replacement logic
-
-				const [parentNode] = node.getParent().getChildren();
-				const startIndex = parentNode.getChildren().indexOf(node);
-				const endIndex = parentNode.getChildren().indexOf(currentNode);
-
-				const newNodeText = [];
-
-				for (const node of parentNode.getChildren().slice(0, startIndex)) {
-					newNodeText.push(node.getFullText());
 				}
 
-				newNodeText.push(block);
+				return identifier + " (" + condition + ") {\n" + compile((statements as string[]).join("\n")) + "}\n";
+			})(currentNode.getFullText());
 
-				for (const node of parentNode.getChildren().slice(endIndex)) {
-					newNodeText.push(node.getFullText());
-				}
+			// SourceFile replacement logic
 
-				console.log(newNodeText);
-				parentNode.replaceWithText(newNodeText.join("\n"));
+			const [parentNode] = node.getParent().getChildren();
+			const startIndex = parentNode.getChildren().indexOf(node);
+			const endIndex = parentNode.getChildren().indexOf(currentNode) + 1;
 
+			const newNodeText = [];
+
+			for (const node of parentNode.getChildren().slice(0, startIndex)) {
+				newNodeText.push(node.getFullText());
+			}
+
+			newNodeText.push(block);
+
+			for (const node of parentNode.getChildren().slice(endIndex)) {
+				newNodeText.push(node.getFullText());
+			}
+
+			console.log(newNodeText);
+			parentNode.replaceWithText(newNodeText.join("\n"));
+
+			if (parentNode.getChildren()[startIndex + 1] !== undefined) {
 				recurse(parentNode.getChildren()[startIndex + 1]);
+			}
 
-				break;
-			case SyntaxKind.SwitchStatement:
-				const [caseBlock] = node.getChildrenOfKind(SyntaxKind.CaseBlock);
+			break;
+		case SyntaxKind.SwitchStatement:
+			const [caseBlock] = node.getChildrenOfKind(SyntaxKind.CaseBlock);
 
-				throw new Error("Not yet implemented.");
+			throw new Error("Not yet implemented.");
 
-				break;
-			default:
-		}
-	});
+			break;
+		default:
+	}
+
+	let hasNextSibling;
+	try {
+		hasNextSibling = node.getNextSibling();
+	} catch (error) {
+		hasNextSibling = false;
+	}
+
+	if (hasNextSibling) {
+		recurse(node.getNextSibling());
+	}
+	//});
 }

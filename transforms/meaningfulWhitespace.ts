0558c2538f1b5ba87a5bee46765c5e6b76672137
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2020-08-23 18:44:41 -0400

    Refactored one test, refactored module

--- transforms/meaningfulWhitespace.ts
+++ transforms/meaningfulWhitespace.ts
@@ -1,21 +1,49 @@
-import { SyntaxKind } from "../lib/typescript/SyntaxKind";
 
 export default function(sourceFile) {
 	const oldText = sourceFile.getFullText();
-	let newText = sourceFile.getFullText().replace("while ", "while (");
-	newText = newText.replace(/:/g, ") {");
-	newText = newText.replace(/elif /g, "} else if (");
-	newText = newText.replace(/ and /g, " && ");
-	newText = newText.replace(/if /g, "if (");
-	newText = newText.replace(/ or /g, " || ");
-	newText = newText.replace(/for /g, "for (");
-	newText = newText.replace(/else /g, "} else {");
-	newText = newText.replace(/ True /g, " true ");
-	if (newText.slice(-1) === "\n") {
-		newText += "}";
-	} else {
-		newText += "\n}";
+	const oldLines = oldText.split("\n");
+	let newLines = [];
+	let singleLine = "";
+	for (let i = 0; i < oldLines.length; i++) {
+		singleLine = oldLines[i].replace("while ", "while (");
+		singleLine = singleLine.replace(/:/g, ") {");
+		singleLine = singleLine.replace(/elif /g, "} else if (");
+		singleLine = singleLine.replace(/ and /g, " && ");
+		singleLine = singleLine.replace(/if /g, "if (");
+		singleLine = singleLine.replace(/ or /g, " || ");
+		singleLine = singleLine.replace(/for /g, "for (");
+		singleLine = singleLine.replace(/else /g, "} else {");
+		singleLine = singleLine.replace(/ True /g, " true ");
+		singleLine = singleLine.replace(/ False /g, " false ");
+
+
+
+		if (singleLine === "") {
+			try {
+				if (newLines[i - 1].split("    ")[0] === "") {
+					singleLine = "}"
+				}
+			} catch (e) {
+				console.log(e)
+			}
+
+		}
+
+		newLines[i] = singleLine;
+
 	}
 
-	sourceFile.replaceWithText(newText);
+	let newLinesStr = newLines.join("\n");
+
+
+	// newText = newText.replace()
+	// if (newText.slice(-1) === "\n") {
+	// 	newText += "}";
+	// } else if (newText.slice(-1) === "") {
+	// 	newText += "\n}";
+	// } else {
+	// 	newText += "\n}";
+	// }
+
+	sourceFile.replaceWithText(newLinesStr);
 }
